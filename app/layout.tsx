50f744b3e04f9ece440514e125b4a0f1ce6eb645
Samuel Adetunji <adetunjisam9@gmail.com>
2026-05-29 00:56:02 +0100

    fix layout type error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+﻿import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -39,7 +39,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
